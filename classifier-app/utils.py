dbc3c005f555654718aa3012b1958144a22ecce3
EstebanMunoz <estebanmunoz@ug.uchile.cl>
2022-10-30 17:01:44 -0300

    Working dataset feature extractor

--- classifier-app/utils.py
+++ classifier-app/utils.py
@@ -1,4 +1,15 @@
+import pickle
+
+import numpy as np
+import torch
+
 ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg'])
+
+dataset_features = np.load('/home/usuario/git-repos/CC6409-Flask/classifier-app/dataset_features.npy')
+dataset_features = torch.from_numpy(dataset_features)
+
+with open('/home/usuario/git-repos/CC6409-Flask/classifier-app/img_names.pickle', 'rb') as data:
+    img_names = pickle.load(data)
 
 
 def allowed_file(filename):
@@ -8,4 +19,9 @@ def allowed_file(filename):
 def get_closest_match(features):
     # ...encontrar la imagen más similar del conjunto objetivo...
     # ...retornar su nombre de archivo...
-    return '1ec96051fd573348_lion.jpg'
+
+    euclidean_dist = (torch.from_numpy(dataset_features) - features).pow(2).sum(1).sqrt()
+    idx_min_dist = euclidean_dist.topk(3, dim=0, largest=False).indices
+    closest_match = [img_names[k] for k in list(idx_min_dist.reshape(-1))]
+
+    return closest_match
